3f84a7e6dd177eaa773de230d8b5691d20613273
prithivirajsr <kingprithiviraj27@gmail.com>
2021-11-04 21:57:57 +0530

    login page implementation

---
+++ src/app/forgotpassword/forgotpassword.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-forgotpassword',
+    templateUrl:'./forgotpassword.component.html',
+    styleUrls:['./forgotpassword.component.css']
+})
+
+export class ForgotpasswordComponent{
+    
+}
