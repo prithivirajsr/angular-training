982ebbcda12a584ea2c50fcfe6f7ab2426a95b0a
prithivirajsr <kingprithiviraj27@gmail.com>
2021-11-05 13:16:04 +0530

    added password reset page and structural directive included

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
 })
 
 export class LoginComponent{
+    hideForgotPassword = false;
+    showForgotPassword(){
+      this.hideForgotPassword = !this.hideForgotPassword;
+    }
+    
     
 }
